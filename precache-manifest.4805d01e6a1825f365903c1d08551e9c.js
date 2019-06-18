3b3a44faa272bbb4dcdd82fdaad5842073f46832
SofiaRuizHinojosa27 <srh_19@hotmail.com>
2019-06-18 10:28:25 -0500

    Updates

---
+++ precache-manifest.4805d01e6a1825f365903c1d08551e9c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab102a9e3a8c74b8186e868a8b807656",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "9e60b2dd5f45a7e619c1",
+    "url": "/portafolio/static/css/main.1a7a1f82.chunk.css"
+  },
+  {
+    "revision": "5608ba5603e90396ac6b",
+    "url": "/portafolio/static/js/2.7ffbc2b6.chunk.js"
+  },
+  {
+    "revision": "9e60b2dd5f45a7e619c1",
+    "url": "/portafolio/static/js/main.b7499786.chunk.js"
+  },
+  {
+    "revision": "9fd29bcc1d4ba73bed23",
+    "url": "/portafolio/static/js/runtime~main.41300385.js"
+  },
+  {
+    "revision": "815522d46c5926d8e9b0c306caeacd97",
+    "url": "/portafolio/static/media/Sofi3.815522d4.jpg"
+  },
+  {
+    "revision": "4d0235c8876a11fc9218fda7c7f32c65",
+    "url": "/portafolio/static/media/SofiPhone.4d0235c8.jpg"
+  },
+  {
+    "revision": "a79d944fbf18e3fbfaa70a5081fb7c95",
+    "url": "/portafolio/static/media/burguerQueen.a79d944f.png"
+  },
+  {
+    "revision": "cccf68434f7980ea372be4936caa292c",
+    "url": "/portafolio/static/media/inBikeMe.cccf6843.png"
+  },
+  {
+    "revision": "db5b16313b3efd637a22c9d1fadfcf4b",
+    "url": "/portafolio/static/media/logo2.db5b1631.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portafolio/./precache-manifest.7da3fcbec99233291ea956d5f3077365.js"
+  "/portafolio/precache-manifest.4805d01e6a1825f365903c1d08551e9c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portafolio/./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portafolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
