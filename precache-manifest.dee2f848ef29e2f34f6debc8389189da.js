4ff2e9c39c03a404e88958c3f4bf557be9342a2a
SofiaRuizHinojosa27 <srh_19@hotmail.com>
2019-06-25 12:48:18 -0500

    Updates

---
+++ precache-manifest.dee2f848ef29e2f34f6debc8389189da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca2b77818bb846b27f8579b591f6d5a",
+    "url": "/sofiaruiz.github.io/index.html"
+  },
+  {
+    "revision": "a641b13a5fcd44e28ecc",
+    "url": "/sofiaruiz.github.io/static/css/main.a42ff92a.chunk.css"
+  },
+  {
+    "revision": "2785dc34fea951569815",
+    "url": "/sofiaruiz.github.io/static/js/2.c2e19106.chunk.js"
+  },
+  {
+    "revision": "a641b13a5fcd44e28ecc",
+    "url": "/sofiaruiz.github.io/static/js/main.f44b5504.chunk.js"
+  },
+  {
+    "revision": "46bbe126f9b9cdd667f0",
+    "url": "/sofiaruiz.github.io/static/js/runtime~main.589f740b.js"
+  },
+  {
+    "revision": "815522d46c5926d8e9b0c306caeacd97",
+    "url": "/sofiaruiz.github.io/static/media/Sofi3.815522d4.jpg"
+  },
+  {
+    "revision": "2cf2450d6b78ec58c1852efa6d395139",
+    "url": "/sofiaruiz.github.io/static/media/SofiPhone.2cf2450d.jpg"
+  },
+  {
+    "revision": "a79d944fbf18e3fbfaa70a5081fb7c95",
+    "url": "/sofiaruiz.github.io/static/media/burguerQueen.a79d944f.png"
+  },
+  {
+    "revision": "cccf68434f7980ea372be4936caa292c",
+    "url": "/sofiaruiz.github.io/static/media/inBikeMe.cccf6843.png"
+  },
+  {
+    "revision": "9439336dc0f7059b7bc0f3183640f461",
+    "url": "/sofiaruiz.github.io/static/media/logo2.9439336d.jpg"
+  },
+  {
+    "revision": "8de837018053077b5747a07c732d03f2",
+    "url": "/sofiaruiz.github.io/static/media/veggie.8de83701.jpg"
+  }
+]);
